f68232e7d0c3e18123f3e1df1d27b77e9f81dacc
Ivo Yakov <outrageousxqt@gmail.com>
2021-08-17 13:47:12 +0300

    Added selenium test demo

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,49 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setup()
+    {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void test()
+    {
+        WebElement usernameTxt = driver.findElement(By.id("username"));
+        usernameTxt.sendKeys("tomsmith");
+        WebElement passwordTxt = driver.findElement(By.id("password"));
+        passwordTxt.sendKeys("SuperSecretPassword!");
+        WebElement submitBtn = driver.findElement(By.className("radius"));
+        submitBtn.click();
+        System.out.println("Current URL is:" + driver.getCurrentUrl());
+        Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
+    }
+
+    @After
+    public void cleanup()
+    {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
