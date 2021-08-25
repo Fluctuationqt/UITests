5f09b06d28f233731c5fad2c258115e4ad51e673
Ivo Yakov <Outrageousxqt@gmail.com>
2021-08-25 15:24:58 +0300

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -5,6 +5,8 @@ import org.junit.experimental.categories.Category;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -30,7 +32,16 @@ public class SeleniumTest {
 
     @Test
     public void test() throws Exception {
-        this.takeSnapShot(driver, "test.jpg");
+        this.takeSnapShot(driver, "test1.jpg");
+        driver.findElement(By.xpath("//*[@title=\"XS Migration\"]")).click();
+        this.takeSnapShot(driver, "test2.jpg");
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@src='../ide-migration/migration-launch.html']")));
+
+        this.takeSnapShot(driver, "test3.jpg");
+        driver.findElement(By.id("host-name")).sendKeys("Doing integration testing!");
+        this.takeSnapShot(driver, "test4.jpg");
     }
 
     @After
@@ -40,11 +51,18 @@ public class SeleniumTest {
         }
     }
 
-    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception {
+    private static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception {
         TakesScreenshot scrShot =((TakesScreenshot)webdriver);
         File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
         File DestFile=new File(fileWithPath);
         System.out.println("Screenshot dir:" + DestFile.getAbsolutePath());
         FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    private void printPageHtml()
+    {
+        var root = driver.findElement(By.xpath("//*"));
+        var page = root.getAttribute("outerHTML");
+        System.out.print(page);
     }
 }
