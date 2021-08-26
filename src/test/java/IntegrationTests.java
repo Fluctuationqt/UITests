18f0ded627cfdea9dca2959f0d420c5128da02e3
IYakov <outrageousxqt@gmail.com>
2021-08-26 15:47:24 +0300

    Chore: Added Firefox test

--- src/test/java/IntegrationTests.java
+++ src/test/java/IntegrationTests.java
@@ -1,2 +1,69 @@
-public interface IntegrationTests {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class IntegrationTests {
+
+    protected WebDriver chrome;
+    protected WebDriver firefox;
+
+    protected void setupChrome(String url)
+    {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+        chrome = new ChromeDriver(options);
+        chrome.navigate().to(url);
+        chrome.manage().window().maximize();
+        chrome.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+    }
+
+    protected void setupFirefox(String url)
+    {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        //options.addArguments("--no-sandbox");
+        //options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("--window-size=1920,1080");
+        firefox = new FirefoxDriver(options);
+        firefox.navigate().to(url);
+        firefox.manage().window().maximize();
+        firefox.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+    }
+
+    protected void cleanupDrivers()
+    {
+        if(chrome != null) chrome.quit();
+        if(firefox != null) firefox.quit();
+    }
+
+    static void takeScreenshot(WebDriver browser, String fileWithPath) throws IOException {
+        TakesScreenshot scrShot =((TakesScreenshot)browser);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile=new File(fileWithPath);
+        System.out.println("[Screenshot saved]: " + DestFile.getAbsolutePath());
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    static void printPageHtml(WebDriver browser) {
+        var root = browser.findElement(By.xpath("//*"));
+        var page = root.getAttribute("outerHTML");
+        System.out.print(page);
+    }
+
 }
--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,68 +1,53 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
+
 import org.junit.*;
 import org.junit.experimental.categories.Category;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 @Category(IntegrationTests.class)
-public class SeleniumTest {
-
-    private WebDriver driver;
+public class SeleniumTest extends IntegrationTests {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        options.addArguments("--window-size=1920,1080");
-        driver = new ChromeDriver(options);
-        driver.navigate().to("http://dirigible:dirigible@127.0.0.1:8080/services/v4/web/ide/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+        String startURL = "http://dirigible:dirigible@127.0.0.1:8080/services/v4/web/ide/";
+        setupChrome(startURL);
+        setupFirefox(startURL);
     }
 
     @Test
-    public void test() throws Exception {
-        this.takeSnapShot(driver, "test1.jpg");
-        driver.findElement(By.xpath("//*[@title=\"XS Migration\"]")).click();
-        this.takeSnapShot(driver, "test2.jpg");
+    public void testChrome() throws Exception {
+        WebDriverWait waitChrome = new WebDriverWait(chrome, 5);
+        waitChrome.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@title=\"XS Migration\"]")));
+        takeScreenshot(chrome, "testChrome_1.jpg");
+        chrome.findElement(By.xpath("//*[@title=\"XS Migration\"]")).click();
+        takeScreenshot(chrome, "testChrome_2.jpg");
+        waitChrome.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@src='../ide-migration/migration-launch.html']")));
+        takeScreenshot(chrome, "testChrome_3.jpg");
+        waitChrome.until(ExpectedConditions.presenceOfElementLocated(By.id("host-name")));
+        chrome.findElement(By.id("host-name")).sendKeys("Doing integration testing!");
+        takeScreenshot(chrome, "testChrome_4.jpg");
+    }
 
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@src='../ide-migration/migration-launch.html']")));
-
-        this.takeSnapShot(driver, "test3.jpg");
-        driver.findElement(By.id("host-name")).sendKeys("Doing integration testing!");
-        this.takeSnapShot(driver, "test4.jpg");
+    @Test
+    public void testFirefox() throws IOException {
+        WebDriverWait waitFirefox = new WebDriverWait(firefox, 5);
+        waitFirefox.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@title=\"XS Migration\"]")));
+        takeScreenshot(firefox, "testFirefox_1.jpg");
+        firefox.findElement(By.xpath("//*[@title=\"XS Migration\"]")).click();
+        takeScreenshot(firefox, "testFirefox_2.jpg");
+        waitFirefox.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@src='../ide-migration/migration-launch.html']")));
+        takeScreenshot(firefox, "testFirefox_3.jpg");
+        waitFirefox.until(ExpectedConditions.presenceOfElementLocated(By.id("host-name")));
+        firefox.findElement(By.id("host-name")).sendKeys("Doing integration testing!");
+        takeScreenshot(firefox, "testFirefox_4.jpg");
     }
 
     @After
     public void cleanup() {
-        if (driver != null) {
-            driver.quit();
-        }
+        cleanupDrivers();
     }
 
-    private static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception {
-        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
-        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
-        File DestFile=new File(fileWithPath);
-        System.out.println("Screenshot dir:" + DestFile.getAbsolutePath());
-        FileUtils.copyFile(SrcFile, DestFile);
-    }
-
-    private void printPageHtml()
-    {
-        var root = driver.findElement(By.xpath("//*"));
-        var page = root.getAttribute("outerHTML");
-        System.out.print(page);
-    }
 }
